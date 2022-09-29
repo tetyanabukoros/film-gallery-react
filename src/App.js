04d88cda5d42163b2f38af2ec905fcd4ffb04cf8
Tania Bukoros <tetiana.bukoros@gmail.com>
2022-09-29 20:15:21 +0300

    start - AppBar created

--- src/App.js
+++ src/App.js
@@ -1,24 +1,85 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+// import 'swiper/swiper.min.css';
+// import 'swiper/components/navigation/navigation.min.css';
+import 'swiper/css';
+import 'swiper/css/navigation';
+
+
+
+
+import { 
+  AppBar, 
+  Container, 
+  Toolbar,
+  Typography,
+  Box,
+  Button,
+  Paper
+} from '@mui/material';
+
+// import { makeStyles } from ' @mui/styles';
+
+// const useStyles = makeStyles((theme) => ({
+//   root: {
+//     flexGrow: 1
+//   },
+//   menuButton: {
+//     marginRight: theme.spacing(1)
+//   },
+//   title: {
+//     flexGrow: 1
+//   }
+// }))
+
+import SwiperCore, {
+  Navigation
+} from 'swiper/core';
+
+SwiperCore.use([Navigation]);
+
+
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <>
+    <AppBar position="fixed" color="primary">
+      <Container fixed >
+        <Toolbar>
+          <Typography marginRight={100}>UserName</Typography>
+          <Box marginRight={3}>
+            <Button color="inherit" variant="outlined">Sign in</Button>
+          </Box>
+          <Button  style={{backgroundColor: '#DC143C'}} variant="contained">Sign up</Button>
+
+        </Toolbar>
+      </Container>
+    </AppBar>
+    
+    <main>
+      <Paper elevation={3}>
+      <Box marginTop={10} paddingTop={5} paddingBottom={5}>
+        <Swiper  navigation={true} className="mySwiper">
+          {['Slide 1', 'Slide 2', 'Slide 3'].map((item, i) => (
+            <SwiperSlide key={i}>
+              <Typography
+                variant={'h6'}
+                align={'center'}
+              >
+                {item}
+              </Typography>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </Box>
+      </Paper>
+
+
+
+
+    </main>
+  </>
   );
 }
 
